a9e1ec4cc52e0e09afe6df3c14c210e9f3bd20e1
Rizky Fajri <mansamussolini@gmail.com>
2022-05-28 16:24:57 +0700

    Singelton

---
+++ Mod11_1302204043/PusatDataSingleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod11_1302204043
+{
+    public class PusatDataSingleton
+    {
+        private static PusatDataSingleton _instance = null;
+        private List<string> DataSimpan;
+
+        private PusatDataSingleton()
+        {
+            this.DataSimpan = new List<string>();
+        }
+        
+        public static PusatDataSingleton GetPusatDataSingleton
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new PusatDataSingleton();
+                }
+                return _instance;
+            }
+        } 
+
+        public List<string> GetSemuaData()
+        {
+            return null;
+        }
+        public void PrintSemuaData()
+        {
+            
+        }
+        public void AddSebuahData(string a)
+        {
+
+        }
+        public void HapusSebuahData(int index)
+        {
+
+        }
+
+
+    }
+}
